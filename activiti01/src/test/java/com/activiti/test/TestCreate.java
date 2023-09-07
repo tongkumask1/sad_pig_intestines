bcff6210ff8d08f2725e9048cf01a657a5c06181
zhangzunjie <15880601002@163.com>
2023-09-07 17:56:17 +0800

    修改

---
+++ activiti01/src/test/java/com/activiti/test/TestCreate.java
@@ -0,0 +1,19 @@
+package com.activiti.test;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.junit.Test;
+
+public class TestCreate {
+    /**
+     * 使用activiti提供的默认方式来创建mysql的表
+     */
+    @Test
+    public void testCreateDbTable(){
+        //需要使用activiti提供的工具类 ProcessEngines 使用方法 getDefaultProcessEngine
+        //getDefaultProcessEngine方法会默认从resources下读取名字为activiti.cfg.xml的文件
+        //创建processEnginc的时候就会创建mysql的表
+        ProcessEngine defaultProcessEngine = ProcessEngines.getDefaultProcessEngine();
+        System.out.println(defaultProcessEngine);
+    }
+}
